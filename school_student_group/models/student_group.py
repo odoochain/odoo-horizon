0b220ed72ec8c6d80691349db44ff82244e9e09d
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-12-03 08:55:30 +0000

    add staff_ids

--- school_student_group/models/student_group.py
+++ school_student_group/models/student_group.py
@@ -64,7 +64,7 @@ class StudentGroup(models.Model):
     short_name = fields.Char(string='Name', compute='_compute_name', store=True) 
     title = fields.Char(string='Title')
 
-    course_ids = fields.Many2many('school.course', 'group_course_rel', 'group_id', 'course_id', string='Courses', domain=lambda(self): "[('teacher_ids', '=', responsible_id)]" if self.type in ['L','F'] else "")
+    course_ids = fields.Many2many('school.course', 'group_course_rel', 'group_id', 'course_id', string='Courses', domain=lambda(self): "['|',('teacher_ids', '=', responsible_id),('teacher_ids', 'in', staff_ids)]" if self.type in ['L','F'] else "")
     
     domain_id = fields.Many2one('school.domain', computed='_compute_course_info', store=True)
     speciality_id = fields.Many2one('school.speciality', computed='_compute_course_info', store=True)
@@ -110,11 +110,11 @@ class StudentGroup(models.Model):
             'domain' : {'individual_course_ids': [('year_id','=',self.year_id.id)]}
         }
     
-    @api.onchange('responsible_id')
+    @api.onchange('responsible_id','staff_ids')
     def onchange_responsible_id(self):
         if self.type in ['L','F'] :
             return {
-                'domain': {'course_ids': [('teacher_ids', '=', self.responsible_id.id)]}
+                'domain': {'course_ids': ['|',('teacher_ids', '=', self.responsible_id.id),('teacher_ids', 'in', self.staff_ids.ids)]}
             }
         
     @api.onchange('course_ids')
