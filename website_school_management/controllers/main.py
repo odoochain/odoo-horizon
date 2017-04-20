ad52b54d9e1a55a5dfb8927c924431378f9e0f66
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-04-20 11:30:00 +0000

    add simple program access for website

---
+++ website_school_management/controllers/main.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+
+from openerp.addons.website.models.website import slug, unslug
+
+from openerp import http
+from openerp.http import request
+from openerp import tools
+from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
+
+class website_portal_school_management(http.Controller):
+
+    @http.route(['/program'], type='http', auth='public')
+    def program(self, redirect=None, **post):
+        programs = request.env['school.program'].sudo().search([('state', '=', 'published')],order="domain_id, cycle_id, name ASC")
+        program_list = []
+        for program in programs:
+            program_list.append({
+                'program' : program,
+                'slug_id' : slug(program),
+            })
+        values = {
+            'program_list': program_list,
+        }
+        return request.render("website_school_management.program", values)
+        
+    @http.route(['/program/<program_id>'], type='http', auth='public')
+    def program_details(self, program_id, redirect=None, **post):
+        _, program_id = unslug(program_id)
+        program = request.registry['school.program'].browse(request.cr, request.uid, program_id, context=request.context)
+        values = {
+            'program': program,
+            'slug_id' : program_id,
+        }
+        return request.render("website_school_management.program_details", values)
+        
+    @http.route(['/print_program/<model("school.program"):program>'], type='http', auth='public')
+    def print_program(self, program, redirect=None, **post):
+        
+        report_obj = request.registry['report']
+        cr, uid, context = request.cr, request.uid, request.context
+        reportname = 'school_management.report_program_details_content'
+        
+        pdf = report_obj.get_pdf(cr, uid, [program.id], reportname, data=None, context=None)
+        pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
+        return request.make_response(pdf, headers=pdfhttpheaders)
---
+++ website_school_management/static/src/js/portal_school.js
@@ -0,0 +1,57 @@
+odoo.define('school_evaluations.action_school_portal_main', function (require) {
+    
+    "use strict";
+
+    var core = require('web.core');
+    var Dialog = require('web.Dialog');
+    var Model = require('web.Model');
+    var session = require('web.session');
+    var Widget = require('web.Widget');
+    
+    var _t = core._t;
+    var QWeb = core.qweb;
+    
+    $(document).ready(function () {
+        // TODO : pas nécessaire de faire ce call get_session_info ??
+        session.rpc("/web/session/get_session_info", {}).then(function(result) {
+            var Users = new Model('res.users');
+            Users.call('has_group', ['school_management.group_teacher']).then(function(has_group){
+                if(has_group){
+                    $("#oe_main_menu_navbar").remove();
+                }
+            });
+        }); 
+    });
+    
+    function setModalMaxHeight(element) {
+      this.$element     = $(element);  
+      this.$content     = this.$element.find('.modal-content');
+      var borderWidth   = this.$content.outerHeight() - this.$content.innerHeight();
+      var dialogMargin  = $(window).width() < 768 ? 20 : 60;
+      var contentHeight = $(window).height() - (dialogMargin + borderWidth);
+      var headerHeight  = this.$element.find('.modal-header').outerHeight() || 0;
+      var footerHeight  = this.$element.find('.modal-footer').outerHeight() || 0;
+      var maxHeight     = contentHeight - (headerHeight + footerHeight);
+    
+      this.$content.css({
+          'overflow': 'hidden'
+      });
+      
+      this.$element
+        .find('.modal-body').css({
+          'max-height': maxHeight,
+          'overflow-y': 'auto'
+      });
+    }
+    
+    $('.modal').on('show.bs.modal', function() {
+      $(this).show();
+      setModalMaxHeight(this);
+    });
+    
+    $(window).resize(function() {
+      if ($('.modal.in').length != 0) {
+        setModalMaxHeight($('.modal.in'));
+      }
+    });
+});
