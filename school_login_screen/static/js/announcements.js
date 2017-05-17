6b096f276c1a8debf5e73d7b32bc4df300f794fd
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-05-17 05:25:50 +0000

    list to card list

--- school_login_screen/static/js/announcements.js
+++ school_login_screen/static/js/announcements.js
@@ -10,10 +10,9 @@ $.ajax({
         $('<div class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp">').append(
             $('<div class="mdl-card__title">').append(
                 announcement.author_avatar ? $('<img alt="Avatar" class="mdl-list__item-avatar">').attr("src","/web/image/res.partner/"+announcement.author_id[0]+"/image_small") : $('<i class="material-icons mdl-list__item-avatar">').text("person"),
-                $('<span>').text(announcement.author_id[1])).append(
+                $('<span>').text(announcement.author_id[1]))).append(
                 $('<div class="mdl-card__supporting-text">').html(announcement.body)
-                )
-        ).appendTo('.announcements-list');
+                ).appendTo('.announcements-list');
     });
     
 });
