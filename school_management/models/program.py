093e102470a4c51e73f5672b3773776a4362cb4e
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-07-04 09:56:59 +0000

    add teacher to cg

--- school_management/models/program.py
+++ school_management/models/program.py
@@ -206,6 +206,8 @@ class CourseGroup(models.Model):
     
     description = fields.Text(string='Description')
     
+    teacher_id = fields.Many2one('res.partner',string='Teacher',domain="[('teacher', '=', '1')]", copy=False)
+    
     course_ids = fields.One2many('school.course', 'course_group_id', string='Courses', copy=True, ondelete="cascade")
 
     bloc_ids = fields.Many2many('school.bloc','school_bloc_course_group_rel', id1='group_id', id2='bloc_id',string='Blocs', copy=False)
