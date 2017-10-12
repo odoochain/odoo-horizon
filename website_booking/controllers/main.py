c370acafb4bd228266448acd5f4d2c77982b8ab8
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-10-12 09:39:05 +0000

    booking policy ressource color

--- website_booking/controllers/main.py
+++ website_booking/controllers/main.py
@@ -117,7 +117,7 @@ class BookingController(http.Controller):
         ]
         all_rooms_ids = request.env['school.asset'].search(
             [['asset_type_id.is_room','=',True],
-             ['room_id.booking_policy','=','available']])
+             ['booking_policy','=','available']])
         busy_rooms_ids = request.env['calendar.event'].with_context({'virtual_id': True}).search(domain,fields)
         busy_rooms_ids = busy_rooms_ids.filtered(lambda r : r.start_datetime <= end).filtered(lambda r : r.stop_datetime >= start).mapped('room_id')
         return (all_rooms_ids - busy_rooms_ids).read(['name'])
