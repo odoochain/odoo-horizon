200e72b0b9ae1f57bffe8010468b2783f53fea3f
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-11-14 20:01:38 +0000

    add annexe 5

--- school_statistics_fields/report/statistics_report.py
+++ school_statistics_fields/report/statistics_report.py
@@ -306,7 +306,7 @@ class Annexe5Xlsx(ReportXlsx):
         for bloc_id in saturn.bloc_ids:
             sheet.write(i, 0, u'ESA')
             sheet.write(i, 1, bloc_id.field_a5.short_name or '')
-            sheet.write(i, 2, bloc_id.source_bloc_section_id.name + '/' + bloc_id.source_bloc_track_id.name + '/' + bloc_id.source_bloc_speciality_id.name)
+            sheet.write(i, 2, bloc_id.source_bloc_section_id.name or '' + '/' + bloc_id.source_bloc_track_id.name or '' + '/' + bloc_id.source_bloc_speciality_id.name or '')
             sheet.write(i, 3, bloc_id.field_a4.name or '')
             sheet.write(i, 4, u'INSCRIPTION 16-17')
             sheet.write(i, 5, u'TYPE')
