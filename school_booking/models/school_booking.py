fc4eaf4bd4ce0a25f2f7055dd1ff422ede42646b
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-03-12 11:30:14 +0000

    Saturn history

--- school_booking/models/school_booking.py
+++ school_booking/models/school_booking.py
@@ -80,7 +80,7 @@ class Event(models.Model):
         
         if student_event in self.categ_ids:
             now = datetime.datetime.now()
-            if now.hour + now.minute / 60 <= 12.5 and (self.start_datetime - fields.Datetime.now()).day > 0 :
+            if now.hour + now.minute / 60 <= 12.5 and (fields.Datetime.from_string(self.start_datetime) - now).day > 0 :
                 raise ValidationError(_("You cannot book for the next day before 12h30."))
             
             duration_list = self.env['calendar.event'].read_group([
