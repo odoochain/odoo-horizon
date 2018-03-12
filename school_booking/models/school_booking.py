efa5c98cdd4078d1c13b305b8262ac6627e9b8dd
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-03-12 11:49:43 +0000

    Saturn history

--- school_booking/models/school_booking.py
+++ school_booking/models/school_booking.py
@@ -80,13 +80,11 @@ class Event(models.Model):
         
         if student_event in self.categ_ids:
             now = datetime.datetime.now()
-            _logger.info(now)
-            _logger.info(now.hour + (1.0*now.minute) / 60)
-            if now.hour + now.minute / 60 <= 11.5 and fields.Datetime.from_string(self.start_datetime).date() != now.date() :
-                raise ValidationError(_("You cannot book for the next day before 12h30."))
+            if now.hour + now.minute / 60 < 11 and fields.Datetime.from_string(self.start_datetime).date() != now.date() :
+                raise ValidationError(_("You cannot book for the next day before 12h00."))
             
-            if now.hour + now.minute / 60 >= 11.5 and fields.Datetime.from_string(self.start_datetime).date() != now.date() and fields.Datetime.from_string(self.start_datetime).date() != now.timedelta(days=1).date() :
-                raise ValidationError(_("You can book only the next day (after 12h30)."))
+            if now.hour + now.minute / 60 >= 11 and fields.Datetime.from_string(self.start_datetime).date() != now.date() and fields.Datetime.from_string(self.start_datetime).date() != now.timedelta(days=1).date() :
+                raise ValidationError(_("You can book only the next day (after 12h00)."))
             
             duration_list = self.env['calendar.event'].read_group([
                     ('user_id', '=', self.user_id.id), ('categ_ids','in',student_event.id)
