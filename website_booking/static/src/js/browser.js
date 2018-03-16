7c8a21c514280cde063e855135b83bec109669e3
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-03-16 04:36:37 +0000

    switch to 30 min minimum

--- website_booking/static/src/js/browser.js
+++ website_booking/static/src/js/browser.js
@@ -238,7 +238,7 @@ var NewBookingDialog = Widget.extend({
                     break;
                 } 
             }
-            if((fromTime.getHours() + fromTime.getMinutes()/60) > (toTime.getHours() + toTime.getMinutes()/60 - 1)) {
+            if((fromTime.getHours() + fromTime.getMinutes()/60) > (toTime.getHours() + toTime.getMinutes()/60 - 0.5)) {
                 self.$('#to_hour').removeClass('valid');
                 self.$('#to_hour').addClass('invalid');
             }
