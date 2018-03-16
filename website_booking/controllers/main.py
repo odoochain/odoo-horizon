a2b5fb3a9d2212943f53c7340ce2fc61d33b1643
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-03-16 04:39:24 +0000

    restrain rooms for regular users

--- website_booking/controllers/main.py
+++ website_booking/controllers/main.py
@@ -116,7 +116,7 @@ class BookingController(http.Controller):
             ('room_id', '<>', False)
         ]
         all_rooms_ids = request.env['school.asset'].search( [['asset_type_id.is_room','=',True]] )
-        busy_rooms_ids = request.env['calendar.event'].with_context({'virtual_id': True}).search(domain,fields)
+        busy_rooms_ids = request.env['calendar.event'].sudo().with_context({'virtual_id': True}).search(domain,fields)
         busy_rooms_ids = busy_rooms_ids.filtered(lambda r : r.start_datetime <= end).filtered(lambda r : r.stop_datetime >= start).mapped('room_id')
         return (all_rooms_ids - busy_rooms_ids).read(['name'])
         
