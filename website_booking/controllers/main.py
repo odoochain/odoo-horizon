39a90f1bb9e12dfd5b4c740c586c76609a8b5abf
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-03-19 14:16:50 +0000

    add help

--- website_booking/controllers/main.py
+++ website_booking/controllers/main.py
@@ -110,18 +110,18 @@ class BookingController(http.Controller):
         start = start.replace('T',' ').replace('Z',' ').replace('.000','').strip()
         end = end.replace('T',' ').replace('Z',' ').replace('.000','').strip()
         fields = ['name','room_id']
-        if int(self_id) > 0 :
+        if self_id == '' :
             domain = [
                 ('start', '<', end),    
                 ('stop', '>', start),
                 ('room_id', '<>', False),
-                ('id', '!=', self_id),
             ]
         else :
             domain = [
                 ('start', '<', end),    
                 ('stop', '>', start),
                 ('room_id', '<>', False),
+                ('id', '!=', self_id),
             ]
         all_rooms_ids = request.env['school.asset'].search( [['asset_type_id.is_room','=',True]] )
         busy_rooms_ids = request.env['calendar.event'].sudo().with_context({'virtual_id': True}).search(domain,fields)
