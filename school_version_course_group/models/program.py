e2d77ba32ec9f5ce3d1be7df887072ba6c4f2e09
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-06-13 13:21:40 +0000

    add version year to course group

---
+++ school_version_course_group/models/program.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+class CourseGroup(models.Model):
+    '''Course Group'''
+    _inherit = 'school.course_group'
+
+    version_year_id = fields.Many2one('school.year', string='Version Year', default=lambda self: self.env.user.current_year_id)
