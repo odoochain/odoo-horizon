a1b4d1ee105862353041bf3cef93645b6b7b012f
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-08-30 12:29:05 +0000

    registration change

--- school_registration/models/individual_program.py
+++ school_registration/models/individual_program.py
@@ -41,9 +41,26 @@ class IndividualProgram(models.Model):
             'student_id' : context.get('default_student_id'),
             'program_id' : context.get('default_program_id'),
             'year_id' :  self.env.user.current_year_id.id,
-            'source_bloc_id' : self.source_program_id.bloc_ids[0].id
+            'source_bloc_id' : self.source_program_id.bloc_ids[0].id,
+            'state' : 'draft',
         })
             
+        cg_ids = []
+        for group in new_pae.source_bloc_id.course_group_ids:
+            # Check if cg already acquiered
+            count = self.env['school.individual_course_group'].search_count([('student_id','=',self.student_id.id),('acquiered','=',true)])
+            if count == 0 :
+                _logger.info('assign course groups : ' + group.name)
+                cg = self.course_group_ids.create({'bloc_id': new_pae.id,'source_course_group_id': group.id, 'acquiered' : 'NA'})
+                courses = []
+                for course in group.course_ids:
+                    _logger.info('assign course : ' + course.name)
+                    courses.append((0,0,{'source_course_id': course.id}))
+                _logger.info(courses)
+                cg.write({'course_ids': courses})
+            else :
+                 _logger.info('skip course groups : ' + group.name)
+
         
         value = {
             'domain': "[]",
