9b3cbb1d98db0e2ffce39994857bd23d9ec68235
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-09-09 09:56:59 +0000

    registration

--- school_evaluations/models/evaluation.py
+++ school_evaluations/models/evaluation.py
@@ -171,6 +171,8 @@ class IndividualProgram(models.Model):
         self.not_acquired_ind_course_group_ids = self.ind_course_group_ids.filtered(lambda ic: ic.acquiered == 'NA')
         self.acquired_ind_course_group_ids = self.ind_course_group_ids.filtered(lambda ic: ic.acquiered == 'A')
         acquired_source_course_group_ids = self.acquired_ind_course_group_ids.mapped('source_course_group_id')
+        _logger.info(acquired_source_course_group_ids)
+        _logger.info(self.source_program_id.course_group_ids)
         self.remaining_course_group_ids = self.source_program_id.course_group_ids - acquired_source_course_group_ids
     
 class IndividualBloc(models.Model):
