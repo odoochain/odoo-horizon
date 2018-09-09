d9a4254317e1ae1ab4e4024539e9631ea11f5487
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-09-09 09:36:40 +0000

    registration

--- school_management/models/program.py
+++ school_management/models/program.py
@@ -125,6 +125,10 @@ class Program(models.Model):
                 course_group_ids = bloc.course_group_ids
         self.course_group_ids = course_group_ids
         
+    bloc1_title = fields.String(compute='_compute_bloc_course_group_ids')
+    bloc2_title = fields.String(compute='_compute_bloc_course_group_ids')
+    bloc3_title = fields.String(compute='_compute_bloc_course_group_ids')
+        
     bloc1_course_group_ids = fields.One2many('school.course_group', string='Courses Groups', compute='_compute_bloc_course_group_ids')
     bloc2_course_group_ids = fields.One2many('school.course_group', string='Courses Groups', compute='_compute_bloc_course_group_ids')
     bloc3_course_group_ids = fields.One2many('school.course_group', string='Courses Groups', compute='_compute_bloc_course_group_ids')
@@ -132,10 +136,13 @@ class Program(models.Model):
     @api.one
     def _compute_bloc_course_group_ids(self):
         if len(self.bloc_ids) > 0 :
+            self.bloc1_title = self.bloc_ids[0].title
             self.bloc1_course_group_ids = self.bloc_ids[0].course_group_ids
         if len(self.bloc_ids) > 1 :
+            self.bloc2_title = self.bloc_ids[1].title
             self.bloc2_course_group_ids = self.bloc_ids[1].course_group_ids
         if len(self.bloc_ids) > 2 :
+            self.bloc3_title = self.bloc_ids[2].title
             self.bloc3_course_group_ids = self.bloc_ids[2].course_group_ids
         
     @api.multi
