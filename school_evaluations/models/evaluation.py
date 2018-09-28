25222614ea1779aa8c19a959d3e95aa3bf279c9c
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-09-28 10:08:10 +0000

    report

--- school_evaluations/models/evaluation.py
+++ school_evaluations/models/evaluation.py
@@ -205,8 +205,7 @@ class IndividualProgram(models.Model):
         self.acquired_ind_course_group_ids = self.ind_course_group_ids.filtered(lambda ic: ic.acquiered == 'A') + self.valuated_course_group_ids
         acquired_source_course_group_ids = self.acquired_ind_course_group_ids.mapped('source_course_group_id')
         self.remaining_course_group_ids = self.source_program_id.course_group_ids - acquired_source_course_group_ids
-        last_bloc = self.bloc_ids[len(self.bloc_ids-1)]
-        self.last_bloc_course_group_ids = last_bloc.course_group_ids
+        self.last_bloc_course_group_ids = self.bloc_ids[-1].course_group_ids
     
 class IndividualBloc(models.Model):
     '''Individual Bloc'''
