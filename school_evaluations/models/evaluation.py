9e95e974a3d653c4c0b79f64d917d3975ac6a545
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-10-02 10:47:05 +0000

    add couse_id to course_ids

--- school_evaluations/models/evaluation.py
+++ school_evaluations/models/evaluation.py
@@ -626,8 +626,8 @@ class IndividualCourseGroup(models.Model):
     def compute_second_session_acquiered(self):
         _logger.debug('Trigger "compute_second_session_acquiered" on Course Group %s' % self.name)
         self.second_session_acquiered = self.first_session_acquiered
-        if self.first_session_deliberated_result_bool:
-            self.first_session_acquiered = 'A'
+        if self.second_session_deliberated_result_bool:
+            self.second_session_acquiered = 'A'
         elif self.enable_exclusion_bool :
             if self.second_session_result >= 10 and (not self.second_session_computed_exclusion_result_bool or self.second_session_deliberated_result_bool):
                 self.second_session_acquiered = 'A'
