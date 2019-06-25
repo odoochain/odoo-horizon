42a434cdff3e69182f8b989385155f15103bfb68
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2019-06-25 08:52:32 +0000

    send report as email

--- school_evaluations/report/evaluation_report.py
+++ school_evaluations/report/evaluation_report.py
@@ -99,7 +99,7 @@ class ReportEvaluationByTeacherWizard(models.TransientModel):
                 
                 report = self.env.ref('school_evaluations.report_evaluation_by_teacher')
                 
-                pdf_bin, _ = report.render_report(teacher_id.id, report.name, data=data)
+                pdf_bin, _ = report.render_report(teacher_id.id, report.template_name, data=data)
                 
                 attachment = self.env['ir.attachment'].create({
                     'name': filename,
