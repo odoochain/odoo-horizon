15dcaa9b6c0131d944348308c4dbbcfcbfbf191d
Jérôme Sonnet <jerome.sonnet@imply.lu>
2019-10-17 19:21:58 +0200

    split contacts

---
+++ school_contacts/models/res_partner.py
@@ -0,0 +1,67 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+import threading
+import re
+
+import openerp
+from openerp import tools, api, fields, models, _
+from openerp.exceptions import UserError
+from openerp.tools.safe_eval import safe_eval
+
+_logger = logging.getLogger(__name__)
+
+class Partner(models.Model):
+    '''Partner'''
+    _inherit = 'res.partner'
+    
+    student = fields.Boolean("Student",default=False)
+    teacher = fields.Boolean("Teacher",default=False)
+    employee = fields.Boolean("Employee",default=False)
+    
+    initials = fields.Char('Initials')
+    
+    @api.one
+    @api.constrains('initials')
+    def _check_initials(self):
+        _logger.info('Chech here !!')
+        if self.initials and not re.match('([A-Z]\.,)*([A-Z]\.)?',self.initials):
+            raise UserError(_("Please encode initials as eg X.,V.,T."))
+    
+    birthplace = fields.Char('Birthplace')
+    phone2 = fields.Char('Phone2')
+    title = fields.Selection([('Mr', 'Monsieur'),('Mme', 'Madame'),('Mlle', 'Mademoiselle')])
+    marial_status = fields.Selection([('M', 'Maried'),('S', 'Single')])
+    registration_date = fields.Date('Registration Date')
+    email_personnel = fields.Char('Email personnel')
+    reg_number = fields.Char('Registration Number')
+    mat_number = fields.Char('Matricule Number')
+    
+    # Secondary adress
+
+    secondary_street = fields.Char('Street')
+    secondary_street2 = fields.Char('Street2')
+    secondary_zip = fields.Char('Zip', size=24, change_default=True)
+    secondary_city = fields.Char('City')
+    secondary_state_id = fields.Many2one("res.country.state", 'State', ondelete='restrict')
+    secondary_country_id = fields.Many2one('res.country', 'Country', ondelete='restrict')
+
+    teacher_curriculum_vitae = fields.Html('Curriculum Vitae')
