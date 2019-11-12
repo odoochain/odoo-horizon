98262ee4ac77cd010fee8d23edab4d5ac8363878
Jérôme Sonnet <jerome.sonnet@imply.lu>
2019-11-12 20:50:49 +0100

    add fields

--- website_horizon_responsive/controllers/main.py
+++ website_horizon_responsive/controllers/main.py
@@ -42,12 +42,12 @@ class BookingLoginController(Home):
 
 class BookingController(http.Controller):
 
-    @http.route('/responsive', type='http', auth='public', website=True)
+    @http.route('/responsive/profile', type='http', auth='public', website=True)
     def booking_browser(self, debug=False, **k):
         values = {
             'user': request.env.user,
         }
-        return request.render('website_horizon_responsive.index', values)
+        return request.render('website_horizon_responsive.profile', values)
         
     # @http.route('/booking/category', type='json', auth='public', website=True)
     # def booking_category(self, id=False, debug=False, **k):
