1f2b89e65a17fcd4ab83698a2ae956e9571993b3
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-01-02 13:44:20 +0100

    add tomorrow bookings

---
+++ website_horizon_responsive/static/src/js/booking-new.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('#from_hour').timepicker({
+        'timeFormat': 'H:i',
+        'minTime': '8:00',
+        'maxTime': '21:30',
+    });
+    $('#from_hour').on('change', function() {
+        var newTime = $('#from_hour').timepicker('getTime');
+        $('#to_hour').timepicker('option', 'minTime', newTime);
+    });
+    $('#to_hour').timepicker({
+        'timeFormat': 'H:i',
+        'minTime': '8:30',
+        'maxTime': '22:00',
+        'showDuration': true,
+    });
+});
