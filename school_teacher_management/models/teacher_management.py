8a3bfbc95930eea50ea0f40e3156a46b0c1e95ab
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-09-30 18:52:08 +0200

    add teacher management

--- school_teacher_management/models/teacher_management.py
+++ school_teacher_management/models/teacher_management.py
@@ -59,8 +59,8 @@ class SchoolTeacherDesignation(models.Model):
     author_id = fields.Many2one('res.partner', string='Auteur', domain="[('type','=','contact')]", required=True, default=lambda self: self.env.user.partner_id)
 
     def _compute_default_number(self):
-    numbers = self.env['school.teacher.designation'].search(['year_id', '=', self.env.user.current_year_id.id])
-    return max(numbers) + 1 or 1
+        numbers = self.env['school.teacher.designation'].search(['year_id', '=', self.env.user.current_year_id.id])
+        return max(numbers) + 1 or 1
 
     dgt_number = fields.Integer(string="DGT PT ESA N°",default=_compute_default_number)
     dgt_state = fields.Selection([('A','Annule'),('R','Remplace'),('C','Complète')],string="Ce DGT",default='R')
