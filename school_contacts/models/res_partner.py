228fc4340c5b434c75ee77ef45e65afb666b692f
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-10-09 00:27:54 +0200

    add historic entries

--- school_contacts/models/res_partner.py
+++ school_contacts/models/res_partner.py
@@ -29,6 +29,47 @@ from openerp.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
+class StudentHistoryEntry(models.Model):
+    '''Student History Entry'''
+    _name = 'school.student_history_entry'
+    
+    year_id = fields.Many2one('school.year', string="Year", required=True)
+    student_id = fields.Many2one('res.partner', string="Student", required=True)
+    activite = fields.Selection([
+        ('0','Enseignement supérieur de la fédération Wallonie Bruxelle'),
+        ('1','Enseignement secondaire non obligatoire (année préparatoire à l’enseignement supérieur, 4e cycle du secondaire, ...)'),
+        ('2','Haute École de la Fédération Wallonie‐Bruxelles ou germanophone'),
+        ('3','Université de la Fédération Wallonie‐Bruxelles'),
+        ('4','Institut supérieur d''Architecture de la Fédération Wallonie‐Bruxelles'),
+        ('5','École supérieure des Arts de la Fédération Wallonie‐Bruxelles'),
+        ('6','Enseignement supérieur de promotion sociale de la Fédération Wallonie‐Bruxelles'),
+        ('7','Enseignement supérieur de la Communauté flamande'),
+        ('8','Enseignement supérieur à l''étranger'),
+        ('9','Travail rémunéré'),
+        ('10','Chômage'),
+        ('11','Autre (année sabbatique, préparation à l''enseignement supérieur autre que dans le cadre de l''enseignement secondaire,...)'),
+        ], string="Activité", required=True)
+    etablissement_id = fields.Many2one('school.stat_etablissement', string="Etablissement")
+    domain_id = fields.Many2one('school.stat_domain', string="Domaine")
+    annee = fields.Selection([
+            ('11','1re Bac'),
+            ('12','2e Bac'),
+            ('13','3e Bac'),
+            ('14','4e Bac'),
+            ('21','1re Master'),
+            ('22','2e Master'),
+            ('23','3e Master'),
+            ('24','4e Master'),
+            ('31','1re Spécialisation'),
+            ('32','2e Spécialisation'),
+            ('40','Doctorat'),
+            ('99','Autres (CAPAES, AESS, Année préparatoire, etc.)'),
+        ], string="Année")
+    resultat = fields.Selection([
+            ('1','Réussite'),
+            ('2','Refusé'),
+        ],string="Résultat")
+
 class Partner(models.Model):
     '''Partner'''
     _inherit = 'res.partner'
@@ -60,6 +101,8 @@ class Partner(models.Model):
     email_personnel = fields.Char('Email personnel')
     reg_number = fields.Char('Registration Number')
     mat_number = fields.Char('Matricule Number')
+    
+    student_historic_entry_ids = fields.One2many('school.student_history_entry', 'student_id', string="Student History Entries", track_visibility='onchange')
     
     # Secondary adress
 
