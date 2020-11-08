089fe1eb903ddc215d1550f0ac9fb01654f6c4dc
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-11-08 09:54:42 +0100

    send email as confirmed

--- school_booking_covid/models/school_booking.py
+++ school_booking_covid/models/school_booking.py
@@ -64,7 +64,7 @@ class Event(models.Model):
                 
             if mail_to_ids:
                 current_user = self.pool['res.users'].browse(cr, uid, uid, context=context)
-                if self.pool['calendar.attendee']._send_mail_to_attendees(cr, uid, mail_to_ids, template_xmlid='calendar_template_meeting_confirmation', email_from=current_user.email, context=context):
+                if self.pool['calendar.attendee']._send_mail_to_attendees(cr, uid, mail_to_ids, template_xmlid='school_booking_covid_template_meeting_confirmation', email_from=current_user.email, context=context):
                     self.message_post(cr, uid, the_id, body=_("A email has been send to specify that the booking is confirmed !"), subtype="calendar.subtype_invitation", context=context)
 
     @api.one
