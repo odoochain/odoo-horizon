eb04c6c59005f0fa3923a3b1e905f97e5869d01a
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-12-28 15:08:34 +0100

    from ue_id to uid

--- school_registration/models/individual_program.py
+++ school_registration/models/individual_program.py
@@ -35,7 +35,7 @@ class IndividualProgram(models.Model):
             # Check if cg already acquiered
             count = self.env['school.individual_course_group'].search_count([('student_id','=',self.student_id.id),('acquiered','=','A'),('source_course_group_id','=',group.id)])
             if count == 0 :
-                _logger.info('Assign course groups : ' + group.ue_id + ' - ' +group.name)
+                _logger.info('Assign course groups : ' + group.uid + ' - ' +group.name)
                 cg = new_pae.course_group_ids.create({'bloc_id': new_pae.id,'source_course_group_id': group.id, 'acquiered' : 'NA'})
                 courses = []
                 for course in group.course_ids:
@@ -44,7 +44,7 @@ class IndividualProgram(models.Model):
                 _logger.info(courses)
                 cg.write({'course_ids': courses})
             else :
-                 _logger.info('Skip course groups : ' + group.ue_id + ' - ' +group.name)
+                 _logger.info('Skip course groups : ' + group.uid + ' - ' +group.name)
     
     
     def register_pae(self):
