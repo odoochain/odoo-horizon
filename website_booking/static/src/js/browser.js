c9a904716ef583831ab9d75c2dafe00c1a687216
Jérôme Sonnet <jerome.sonnet@imply.lu>
2021-01-29 13:54:36 +0100

    add goto date

--- website_booking/static/src/js/browser.js
+++ website_booking/static/src/js/browser.js
@@ -779,7 +779,7 @@ var Browser = Widget.extend({
         
         "click #goto-date-button": function (event) {
             var self = this;
-            this.cal.goto_date(this.$('.datepicker').val);
+            this.cal.goto_date(moment(this.$('.datepicker').val,'dd/mm/yyyy'));
         },
         
     },
