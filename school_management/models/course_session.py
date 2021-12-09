21c9d16692825ee8bdd9eb3a94b88bced4daed70
Jérôme Sonnet <jerome.sonnet@imply.lu>
2021-12-09 07:34:40 +0100

    fix group_by

--- school_management/models/course_session.py
+++ school_management/models/course_session.py
@@ -18,7 +18,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import res_partner
-from . import program
-from . import individual_program
-from . import course_session
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+class CourseSession(models.Model):
+    '''Course Session'''
+    _name = 'school.course_session'
+    _inherit = ['mail.thread']
+    
+    title = fields.Char(related="course_id.title")
+    
+    year_id = fields.Many2one('school.year', string='Year', required=True)
+    course_id = fields.Many2one('school.course', string='Course', required=True)
+    teacher_id = fields.Many2one('res.partner', string='Teacher', domain="[('teacher', '=', '1')]")
+    
+    quarter = fields.Selection([('q1', 'Q1'),('q2', 'Q2')],string='Quarter')
+    schedule = fields.Text(string='Schedule')
+    room = fields.Text(string='Room')
+    
+    student_ids = fields.Many2many('res.partner','school_course_session_student_rel', 'course_session_id', 'student_id', string='Students')
