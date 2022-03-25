f0ae9ce9e92e88fa79c84dc35ec894254ebb5112
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-03-25 08:51:30 +0100

    fix mobile booking

--- website_booking/static/src/js/browser.js
+++ website_booking/static/src/js/browser.js
@@ -213,10 +213,10 @@ var NewBookingDialog = Widget.extend({
                         ],
                     }).then(id => {
                         self.trigger_up('updateEvent', {'id': id});
-                        self.parent.main_modal.modal('hide');
+                        self.parent.main_modal.modal('close');
                     }).catch(error => {
                         Materialize.toast(error.message.data.message, 4000)
-                        self.parent.main_modal.modal('hide');
+                        self.parent.main_modal.modal('close');
                     });
                 } else {
                     rpc.query({
