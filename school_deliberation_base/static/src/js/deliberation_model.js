6290fe9ac55402bced5bffc9dd99f1813068bbdb
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-04-16 08:25:47 +0200

    add deliberation tool

--- school_deliberation_base/static/src/js/deliberation_model.js
+++ school_deliberation_base/static/src/js/deliberation_model.js
@@ -7,19 +7,47 @@ odoo.define('deliberation.DeliberationModel', function (require) {
     var DeliberationModel = BasicModel.extend({
 
         /**
+         * Applies the forecast logic to the domain and context if needed before the read_group.
+         * After the read_group, checks the end date of the last displayed group in order to be able to
+         * add the next group with the ForecastColumnQuickCreate widget
+         *
          * @private
-         * @param {Object} record
-         * @param {string} fieldName
+         * @override
+         */
+        async __load(params) {
+            var self = this;
+            return this._super(...arguments).then(function () {
+                self._fetchProgram()
+            });
+        },
+        
+        /**
+         * Applies the forecast logic to the domain and context if needed before the read_group.
+         * After the read_group, checks the end date of the last displayed group in order to be able to
+         * add the next group with the ForecastColumnQuickCreate widget
+         *
+         * @private
+         * @override
+         */
+        async __reload(id, params) {
+            var self = this;
+            return this._super(...arguments).then(function () {
+                self._fetchProgram()
+            });
+        },
+
+        /**
+         * @private
          * @returns {Promise}
          */
-        _fetchProgram: function (record, fieldName) {
+        _fetchProgram: function () {
+            var self = this;
             return this._rpc({
-                model: 'school.individual_program',
-                method: 'search_read',
-                domain: [['id','=',record.program_id]],
-                fields: [],
-            }).then(function (result) {
-                return result;
+                model: "school.individual_program", 
+                method: "read", 
+                args: [[this.state.data.program_id.res_id]]
+            }).then(result => { 
+                self.state.program = result;
             });
         },
           
