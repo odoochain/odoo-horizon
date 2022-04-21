7f9f99968e48d27ed1db9b253b661280911afa51
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-04-21 14:20:36 +0200

    improve deliberation

--- school_deliberation_base/static/src/js/deliberation_controller.js
+++ school_deliberation_base/static/src/js/deliberation_controller.js
@@ -23,13 +23,14 @@ odoo.define('deliberation.DeliberationController', function (require) {
 
         _onClose: function (event) {
             event.stopPropagation();
+            var self = this;
             this._rpc({
                 model:'school.individual_bloc',
                 method:'close_deliberate_bloc',
                 args: [ '' ],
                 context: this.initialState.context,
             }).then(result => {
-                return result;
+                self.do_action(result);
             })
         },
 
