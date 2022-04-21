809c27439309705eab542158670ce33d69a75236
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-04-21 14:06:11 +0200

    improve deliberation

--- school_deliberation_base/static/src/js/deliberation_controller.js
+++ school_deliberation_base/static/src/js/deliberation_controller.js
@@ -22,9 +22,11 @@ odoo.define('deliberation.DeliberationController', function (require) {
         },
 
         _onClose: function (event) {
+            event.stopPropagation();
             return this._rpc({
                 model:'school.individual_bloc',
                 method:'close_deliberate_bloc',
+                args: [ ],
             });
         },
 
