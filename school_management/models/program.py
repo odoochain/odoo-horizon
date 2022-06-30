365ac98d80769dddc072fdca5dacbf1a9a6dc097
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-06-30 17:31:20 +0200

    Add code and info to programs

--- school_management/models/program.py
+++ school_management/models/program.py
@@ -101,8 +101,6 @@ class Program(models.Model):
     
     speciality_id = fields.Many2one('school.speciality', string='Speciality')
     
-    program_code = fields.Char(required=True, string='Program Code', size=10)
-    
     ares_code = fields.Char(required=True, string='ARES Code', size=10)
     
     graca_code = fields.Char(required=True, string='GRACA Code', size=10)
