41fd59d438c86e8a9cfd740e8ff9fdbd9dde357b
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-07-05 11:51:09 +0200

    edit PAE in kanban view

--- school_management/models/individual_program.py
+++ school_management/models/individual_program.py
@@ -342,7 +342,7 @@ class IndividualCourseGroup(models.Model):
     source_course_group_uid = fields.Char(related='source_course_group_id.uid', string="Source Course Group UID")
     source_course_group_responsible_id = fields.Many2one('res.partner', related='source_course_group_id.responsible_id', string="Source Course Group Responsible")
     
-    bloc_id = fields.Many2one('school.individual_bloc', string="Bloc", ondelete='cascade', readonly=True)
+    bloc_id = fields.Many2one('school.individual_bloc', string="Bloc", ondelete='cascade')
 
     source_bloc_id = fields.Many2one('school.bloc', string="Source Bloc", related='bloc_id.source_bloc_id', readonly=True, store=True)
     source_bloc_name = fields.Char(related='bloc_id.source_bloc_name', string="Source Course Bloc Name", readonly=True, store=True)
