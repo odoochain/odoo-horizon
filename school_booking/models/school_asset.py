5d93065540b8e93dcd699c0b2250e9065443cd68
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-09-07 10:17:37 +0200

    Fix images

--- school_booking/models/school_asset.py
+++ school_booking/models/school_asset.py
@@ -28,7 +28,7 @@ class Asset(models.Model):
     '''School Asset'''
     _name = 'school.asset'
     _description = 'School Asset'
-    _inherit = ['mail.thread','image.mixin',]
+    _inherit = ['mail.thread','image.mixin']
     _order = 'sequence'
     
     booking_policy = fields.Selection([
@@ -155,6 +155,7 @@ class Building(models.Model):
     """ Asset Type """
     _name = 'school.building'
     _description = 'Building'
+    _inherit = ['image.mixin']
 
     name = fields.Char('Name', required=True, translate=True)
     location = fields.Char('Location', translate=True)
@@ -186,15 +187,3 @@ class Building(models.Model):
     sunday = fields.Boolean(string="Sunday", default=False)
     sunday_from = fields.Float(string="Sunday From", default=0.0)
     sunday_to = fields.Float(string="Sunday From", default=0.0)
-    
-    # image: all image fields are base64 encoded and PIL-supported
-    image = fields.Binary("Photo", attachment=True,
-        help="This field holds the image used as photo for the employee, limited to 1024x1024px.")
-    image_medium = fields.Binary("Medium-sized photo", attachment=True,
-        help="Medium-sized photo of the employee. It is automatically "
-             "resized as a 128x128px image, with aspect ratio preserved. "
-             "Use this field in form views or some kanban views.")
-    image_small = fields.Binary("Small-sized photo", attachment=True,
-        help="Small-sized photo of the employee. It is automatically "
-             "resized as a 64x64px image, with aspect ratio preserved. "
-             "Use this field anywhere a small image is required.")
