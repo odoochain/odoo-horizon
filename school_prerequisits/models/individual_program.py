64489ffca39787d8ec01d474a1afc46c0817d142
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-09-16 11:43:36 +0200

    Disable Pre-Co requis until fix

--- school_prerequisits/models/individual_program.py
+++ school_prerequisits/models/individual_program.py
@@ -31,27 +31,29 @@ class IndividualBloc(models.Model):
     
     @api.onchange('course_group_ids')
     def on_change_course_group_ids(self):
-        self.ensure_one()
-        res = {}
-        current_scg_ids = self.course_group_ids.mapped('source_course_group_id')
-        for cg in self.course_group_ids:
-            scg = cg.source_course_group_id
-            # Check pre-requisits
-            for prereq in scg.pre_requisit_course_ids:
-                _logger.info('PreRequisit %s match for %s in %s' % (prereq.id, scg.id, cg.id))
-                icg_acq = self.env['school.individual_course_group'].search([('student_id','=',self.student_id.id),('source_course_group_id','=',prereq.id),('acquiered','=',True)])
-                if len(icg_acq) == 0 :
-                    res = {'warning': {
-                        'title': _('Warning'),
-                        'message': _("PreRequisit for course group %s is not matched" % prereq.name)
-                    }}
-            # Check co-requisits
-            for coreq in scg.co_requisit_course_ids:
-                icg_acq = self.env['school.individual_course_group'].search([('student_id','=',self.student_id.id),('source_course_group_id','=',coreq.id)])
-                if len(icg_acq) == 0 :
-                    res = {'warning': {
-                        'title': _('Warning'),
-                        'message': _("CoRequisit for course group %s is not matched" % coreq.name)
-                    }}
-        if res:
-            return res
+        pass
+    
+        # self.ensure_one()
+        # res = {}
+        # current_scg_ids = self.course_group_ids.mapped('source_course_group_id')
+        # for cg in self.course_group_ids:
+        #     scg = cg.source_course_group_id
+        #     # Check pre-requisits
+        #     for prereq in scg.pre_requisit_course_ids:
+        #         _logger.info('PreRequisit %s match for %s in %s' % (prereq.id, scg.id, cg.id))
+        #         icg_acq = self.env['school.individual_course_group'].search([('student_id','=',self.student_id.id),('source_course_group_id','=',prereq.id),('acquiered','=',True)])
+        #         if len(icg_acq) == 0 :
+        #             res = {'warning': {
+        #                 'title': _('Warning'),
+        #                 'message': _("PreRequisit for course group %s is not matched" % prereq.name)
+        #             }}
+        #     # Check co-requisits
+        #     for coreq in scg.co_requisit_course_ids:
+        #         icg_acq = self.env['school.individual_course_group'].search([('student_id','=',self.student_id.id),('source_course_group_id','=',coreq.id)])
+        #         if len(icg_acq) == 0 :
+        #             res = {'warning': {
+        #                 'title': _('Warning'),
+        #                 'message': _("CoRequisit for course group %s is not matched" % coreq.name)
+        #             }}
+        # if res:
+        #     return res
