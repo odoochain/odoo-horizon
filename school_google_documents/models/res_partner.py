ffa015c9ad66363787c598551fe54e437712d06e
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-11-10 06:43:06 +0100

    school_google_documents first commit

--- school_google_documents/models/res_partner.py
+++ school_google_documents/models/res_partner.py
@@ -10,4 +10,4 @@ _logger = logging.getLogger(__name__)
 class Partner(models.Model):
     '''Partner'''
     _name = 'res.partner'
-    _inherit = ['res.partner', 'google_drive_folder']
+    _inherit = ['res.partner', 'school.google_drive_folder']
