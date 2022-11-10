bf50f4d97543b381782a42e5b0d19e53a02903d2
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-11-10 06:44:18 +0100

    school_google_documents first commit

--- school_google_documents/models/__init__.py
+++ school_google_documents/models/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import res_partner
 from . import google_drive_service
+from . import res_partner
