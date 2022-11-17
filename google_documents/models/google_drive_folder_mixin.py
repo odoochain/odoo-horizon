56efa1527c4b832616b35046da0bdd9ab55b1b1b
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-11-17 09:18:02 +0100

    remove other modules dep

--- google_documents/models/google_drive_folder_mixin.py
+++ google_documents/models/google_drive_folder_mixin.py
@@ -38,10 +38,7 @@ class GoogleDriveFolderMixin(models.AbstractModel):
     google_drive_folder_id = fields.Char(string="Google Drive Folder Id",copy=False)
     
     def _compute_google_drive_connected(self):
-        if self.google_drive_folder_id :
-            self.google_drive_connected = self.env['google.service'].is_google_drive_connected()
-        else :
-            self.google_drive_connected = False
+        self.google_drive_connected = self.env.company.google_drive_id and self.env.company.google_drive_id.is_google_drive_connected()
         
     google_drive_connected = fields.Boolean(string="Is Google Drive Connected", compute=_compute_google_drive_connected)
 
