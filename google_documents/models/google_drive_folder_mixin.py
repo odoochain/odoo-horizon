aa34273a103c74e7363ce18a7629b7bce6ea5dc2
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-11-17 10:25:11 +0100

    remove other modules dep

--- google_documents/models/google_drive_folder_mixin.py
+++ google_documents/models/google_drive_folder_mixin.py
@@ -93,38 +93,43 @@ class GoogleDriveService(models.Model):
     drive_ttl = fields.Float('Drive Token TTL')
     drive_token_validity = fields.Datetime('Token Validity')
     
+    def action_connect_to_drive(self):
+        self.ensure_one()
+        
+        # Use the client_secret.json file to identify the application requesting
+        # authorization. The client ID (from that file) and access scopes are required.
+        flow = google_auth_oauthlib.flow.Flow.from_client_config(
+            client_config=json.loads(self.drive_client_config_json),
+            scopes=self._get_drive_scope())
+        
+        # Indicate where the API server will redirect the user after the user completes
+        # the authorization flow. The redirect URI is required. The value must exactly
+        # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
+        # configured in the API Console. If this value doesn't match an authorized URI,
+        # you will get a 'redirect_uri_mismatch' error.
+        flow.redirect_uri = self._get_redirect_uri()
+        
+        # Generate URL for request to Google's OAuth 2.0 server.
+        # Use kwargs to set optional request parameters.
+        authorization_url, state = flow.authorization_url(
+            # Enable offline access so that you can refresh an access token without
+            # re-prompting the user for permission. Recommended for web server apps.
+            access_type='offline',
+            # Enable incremental authorization. Recommended as a best practice.
+            include_granted_scopes='true')
+        
+        _logger.info('STATE : %s' % state)
+        
+        return {
+              'type'     : 'ir.actions.act_url',
+              'url'      : authorization_url
+        }
+    
     def is_google_drive_connected(self):
         self.ensure_one()
         
         if not self.drive_auth_code:
-            # Use the client_secret.json file to identify the application requesting
-            # authorization. The client ID (from that file) and access scopes are required.
-            flow = google_auth_oauthlib.flow.Flow.from_client_config(
-                client_config=json.loads(self.drive_client_config_json),
-                scopes=self._get_drive_scope())
-            
-            # Indicate where the API server will redirect the user after the user completes
-            # the authorization flow. The redirect URI is required. The value must exactly
-            # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
-            # configured in the API Console. If this value doesn't match an authorized URI,
-            # you will get a 'redirect_uri_mismatch' error.
-            flow.redirect_uri = self._get_redirect_uri()
-            
-            # Generate URL for request to Google's OAuth 2.0 server.
-            # Use kwargs to set optional request parameters.
-            authorization_url, state = flow.authorization_url(
-                # Enable offline access so that you can refresh an access token without
-                # re-prompting the user for permission. Recommended for web server apps.
-                access_type='offline',
-                # Enable incremental authorization. Recommended as a best practice.
-                include_granted_scopes='true')
-            
-            _logger.info('STATE : %s' % state)
-            
-            return {
-                  'type'     : 'ir.actions.act_url',
-                  'url'      : authorization_url
-            }
+            raise UserError('Google Drive Not Connected - please contact your administrator.')
         
         if not self.drive_refresh_token :
             base_url = self.env.user.get_base_url()
