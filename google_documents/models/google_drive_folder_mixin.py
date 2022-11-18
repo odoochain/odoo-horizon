77100670d71712d997e7f3d91e4239f6b3a05155
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-11-18 11:08:19 +0100

    remove unused domain

--- google_documents/models/google_drive_folder_mixin.py
+++ google_documents/models/google_drive_folder_mixin.py
@@ -135,7 +135,9 @@ class GoogleDriveService(models.Model):
         drive = googleapiclient.discovery.build(
         API_SERVICE_NAME, API_VERSION, credentials=self._get_credential())
         
-        files = drive.files().children().list(folderId=folderId).execute()
+        _logger.info('HERE')
+        
+        files = drive.files().list(folderId=folderId).execute()
         
         _logger.info('FILES : %s ' % files)
         
