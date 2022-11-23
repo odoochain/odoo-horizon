e2b1f73bb52a73169a12fdf925679ac24459fc67
Jerome Sonnet <jerome.sonnet@gmail.com>
2022-11-23 16:18:09 +0100

    improve reporting

--- school_registration/models/individual_program.py
+++ school_registration/models/individual_program.py
@@ -33,8 +33,7 @@ class IndividualBloc(models.Model):
     
     def _compute_student_signature_date(self):
         for rec in self:
-            att = self.env['ir.attachment'].search_read([['res_model','=','school.individual_bloc'],['res_id','=',rec.id]], fields=['__last_update'], limit=1)
-            rec.student_signature_date = att[0].__last_update if att else None
+            rec.student_signature_date = rec.student_signature.__last_update
             
     student_signature_date = fields.Date(string="Student Signature Date", compute=_compute_student_signature_date)
 
