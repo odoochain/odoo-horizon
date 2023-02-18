bc15dc43ee0f4081de733362b233e4585bbaa6a6
Jerome Sonnet <jerome.sonnet@gmail.com>
2023-02-18 10:25:35 +0100

    implement forms

--- school_management/models/program.py
+++ school_management/models/program.py
@@ -196,7 +196,7 @@ class Bloc(models.Model):
     
     level = fields.Selection([('0','Free'),('1','Bac 1'),('2','Bac 2'),('3','Bac 3'),('4','Master 1'),('5','Master 2'),('6','Agregation'),],string='Level')
  
-    domain = fields.Selection(related='program_id.domain', string='Domain',store=True)   
+    domain = fields.Char(related='program_id.domain', string='Domain',store=True)   
     speciality_id = fields.Many2one(related='program_id.speciality_id', string='Speciality',store=True)
  
     bloc_group = fields.Char(string='Bloc Group', size=10)
