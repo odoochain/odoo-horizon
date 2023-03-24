3d18f1fffe66e9127ead56afede664b89515186a
Jerome Sonnet <jerome.sonnet@gmail.com>
2023-03-24 06:06:48 +0100

    fix report

--- school_reporting_xlsx/report/school_report_xlsx.py
+++ school_reporting_xlsx/report/school_report_xlsx.py
@@ -110,6 +110,6 @@ class RegistrationExportXlsx(models.AbstractModel):
         for row_num, row_data in enumerate(df.values):
             for col_num, col_data in enumerate(row_data):
                 if isinstance(col_data, list):
-                    worksheet.write(row_num + 1, col_num, ','.join(col_data))
+                    worksheet.write(row_num + 1, col_num, json.dumps(col_data, indent=2))
                 else : 
                     worksheet.write(row_num + 1, col_num, col_data)
