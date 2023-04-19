e44f7ecc26d5746cfd4a7a2d92f2414728d2a605
ito-invest-js <jerome.sonnet@ito-invest.lu>
2023-04-19 06:42:02 +0000

    first impl

--- school_webservices/models/school_webservices.py
+++ school_webservices/models/school_webservices.py
@@ -92,7 +92,7 @@ class FaseService(models.Model):
     '''Fase Web Service'''
     _inherit = 'school.webservice'
 
-    def action_test_fase(self):
+    def action_test_service(self):
         self.doRequest(self.env.context['allowed_company_ids'][0])
 
     def __callOperation(self, client, record=False):
