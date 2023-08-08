482ffaa6a9a9aa06359a5807a86d0451727d44e9
Bastien Vizzini <bv@neodiensis.com>
2023-08-08 11:46:57 +0200

    Modified access for reservations

--- website_school_portal/controllers/controllers.py
+++ website_school_portal/controllers/controllers.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+from odoo.http import (Response)
 
 import logging
 
@@ -31,20 +32,24 @@ class WebSchoolPortalController(http.Controller):
 
     @http.route('/mes-donnees/mon-programme', type='http', auth='user', website=True) # TODO >>> website_school_portal >>> controller mes-donnees >>> route /mes-donnees/mon-programme
     def responsive_blocs(self, debug=False, **k):
+        if not (request.env.user.partner_id.student or request.env.is_admin()):
+            return Response(template='website_school_portal.hz_page_403', status=403)
         values = {
             'user': request.env.user,
-            'blocs': request.env['school.individual_bloc'].search([('student_id','=',request.env.user.partner_id.id),('year_id','=',request.env.user.current_year_id.id)]),
+            'blocs': request.env['school.individual_bloc'].sudo().search([('student_id','=',request.env.user.partner_id.id),('year_id','=',request.env.user.current_year_id.id)]),
             'display_results': request.env['ir.config_parameter'].sudo().get_param('school.display.results', '0'),
         }
         return request.render('website_school_portal.hz_blocs', values)
     
     @http.route('/mes-donnees/mes-documents', type='http', auth='user', website=True) # TODO >>> website_school_portal >>> controller mes-donnees >>> route /mes-donnees/mes-documents
     def responsive_documents(self, debug=False, **k):
+        if not (request.env.user.partner_id.student or request.env.is_admin()):
+            return Response(template='website_school_portal.hz_page_403', status=403)
         user = request.env.user
         partner_id = user.partner_id
         values = {
             'user': user,
             'official_document_ids' : partner_id.official_document_ids,
-            'blocs' : request.env['school.individual_bloc'].search([('student_id','=',request.env.user.partner_id.id),('year_id','=',request.env.user.current_year_id.id)]),
+            'blocs' : request.env['school.individual_bloc'].sudo().search([('student_id','=',request.env.user.partner_id.id),('year_id','=',request.env.user.current_year_id.id)]),
         }
         return request.render('website_school_portal.hz_documents', values)
